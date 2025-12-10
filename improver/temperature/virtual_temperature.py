8a68370f03fae776295abc4dea42138b12c7af81
Katherine Tomkins <katherine.tomkins@metoffice.gov.uk>
2025-12-10 18:31:25 +0000

    fix not working in this instance

--- improver/temperature/virtual_temperature.py
+++ improver/temperature/virtual_temperature.py
@@ -36,8 +36,7 @@ class VirtualTemperature(BasePlugin):
         # The units get lost when being calculated as part of running a graph
         # using the multiprocessing scheduler in dagrunner and so need to be
         # added back after the calculation on line 33.
-        # Now fixed: https://github.com/SciTools/cf-units/pull/603
-        # virtual_temperature.units = str(virtual_temperature.units)
+        virtual_temperature.units = str(virtual_temperature.units)
 
         # Update the cube metadata
         virtual_temperature.rename("virtual_temperature")
